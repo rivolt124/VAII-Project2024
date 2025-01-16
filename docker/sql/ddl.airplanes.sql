ab5924e1f8f689a5567a35f1976277c5ec7c2c7f
rivolt124 <richard.vymazal@gmail.com>
2025-01-16 13:40:36 +0100

    DB updates

--- docker/sql/ddl.airplanes.sql
+++ docker/sql/ddl.airplanes.sql
@@ -9,10 +9,26 @@ DROP TABLE IF EXISTS `airplanes`;
 CREATE TABLE `airplanes`
 (
     `id`            INT                 NOT NULL AUTO_INCREMENT,
-    `registration`  VARCHAR(20)         NOT NULL,
+    `registration`  VARCHAR(10)         NOT NULL,
+    `type`          CHAR(4)             NOT NULL,
     `picture`       VARCHAR(200)        NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `registration_unique` (`registration`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
+INSERT INTO `airplanes` (`registration`, `type`, `picture`)
+VALUES
+    ('BGA113Y', 'A337','https://cdn.jetphotos.com/full/6/1451541_1733068038.jpg'),
+    ('UAE34Y', 'A388','https://cdn.jetphotos.com/full/5/411647_1732103504.jpg'),
+    ('E10E3', 'LJ35', 'https://cdn.jetphotos.com/full/6/2197953_1710963191.jpg'),
+    ('NJE478E', 'CL35', 'https://cdn.jetphotos.com/full/5/887251_1710697543.jpg'),
+    ('KLM37M', 'E75L', 'https://cdn.jetphotos.com/full/5/1147658_1727893519.jpg'),
+    ('UAE33T', 'A388', 'https://cdn.jetphotos.com/full/5/1220365_1729726301.jpg'),
+    ('AFR194', 'A359', 'https://cdn.jetphotos.com/full/6/805925_1717462972.jpg'),
+    ('SXS2FC', 'B738', 'https://cdn.jetphotos.com/full/6/747549_1717952868.jpg'),
+    ('CCA720', 'B789', 'https://cdn.jetphotos.com/full/5/656930_1734011619.jpg'),
+    ('ITY159', 'A320', 'https://cdn.jetphotos.com/full/6/727753_1732132419.jpg'),
+    ('BAW198', 'B772', 'https://cdn.jetphotos.com/full/5/698951_1720253024.jpg'),
+    ('QTR780', 'B77L', 'https://cdn.jetphotos.com/full/6/935219_1727130566.jpg'),
+    ('OYA5545', 'B772', 'https://cdn.jetphotos.com/full/6/1341932_1731053525.jpg');
--- docker/sql/ddl.schedules.sql
+++ docker/sql/ddl.schedules.sql
@@ -10,10 +10,30 @@ CREATE TABLE `schedules`
 (
     `id`            INT                 NOT NULL AUTO_INCREMENT,
     `flight_number` varchar(6)          NOT NULL,
-    `date`          date                NOT NULL,
+    `date`          date                NOT NULL DEFAULT '0000-00-00',
     PRIMARY KEY (`id`),
     UNIQUE KEY `flight_date_unique` (`flight_number`, `date`),
     CONSTRAINT `fk_flight_number` FOREIGN KEY (`flight_number`) REFERENCES `flights` (`flight_number`) ON DELETE CASCADE
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
+INSERT INTO `schedules` (`flight_number`, `date`)
+VALUES
+    ('BA101', '2025-01-20'),
+    ('LH102', '2025-01-20'),
+    ('AF203', '2025-01-21'),
+    ('EK104', '2025-01-21'),
+    ('SQ105', '2025-01-22'),
+    ('AA206', '2025-01-22'),
+    ('QF207', '2025-01-23'),
+    ('CX208', '2025-01-23'),
+    ('DL209', '2025-01-24'),
+    ('UA210', '2025-01-24'),
+    ('NZ211', '2025-01-25'),
+    ('LS212', '2025-01-25'),
+    ('OS213', '2025-01-26'),
+    ('TK214', '2025-01-26'),
+    ('BA301', '2025-01-27'),
+    ('LH302', '2025-01-27'),
+    ('AF303', '2025-01-28'),
+    ('EK304', '2025-01-28');
