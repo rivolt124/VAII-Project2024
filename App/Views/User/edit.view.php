eb83b33d690a51edafed028aaa140b0d47c6200e
rivolt124 <richard.vymazal@gmail.com>
2025-02-05 16:34:58 +0100

    edit user

---
+++ App/Views/User/edit.view.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \App\Core\LinkGenerator $link */
+/** @var Array $data */
+
+$showFooter = false;
+?>
+
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-6 d-flex gap-4  flex-column">
+            <h1>Edit your account</h1>
+
+            <?php require 'form.view.php' ?>
+
+        </div>
+    </div>
+</div>
